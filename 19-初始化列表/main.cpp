ddd6ce681b327395d8de4eb981108e67f3b9e0b7
steve xiaohu zhao <steve_zhao@163.com>
2025-10-10 16:24:51 +0800

    构造函数和析构函数的执行顺序

--- 19-初始化列表/main.cpp
+++ 19-初始化列表/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // 初始化列表
@@ -25,12 +26,57 @@ class Person {
     }
 };
 
+//// 思考A和B的构造函数和析构函数执行顺序
+// class A {
+//
+// };
+//
+// class B {
+//     A a;
+// }
+
+class Phone {
+  public:
+    // 手机品牌
+    string m_brand;
+    Phone(string brand) {
+        m_brand = brand;
+        cout << "Phone 的构造函数被执行了！" << endl;
+    }
+
+    ~Phone() { cout << "Phone 的析构函数" << endl; }
+};
+
+class Student {
+  public:
+    Student(string name, string brand) : m_name(name), m_phone(brand) {
+        cout << "Student 的构造函数被执行了！" << endl;
+    }
+    string m_name;
+    Phone m_phone;
+
+    void show() {
+        cout << "姓名：" << m_name << " - 手机品牌：" << m_phone.m_brand
+             << endl;
+    }
+
+    ~Student() { cout << "Student 的析构函数 " << endl; }
+};
+
 int main() {
     // 1、通过传统调用构造函数初始化属性
     Person p(10, 20, 30);
 
     //    // 2、通过初始化列表初始化属性
     //    Person p;
+
+    // 3、类作为对象的成员
+    Student s1("steve", "iPhone");
+    cout << "---------------------" << endl;
+    s1.show();
     p.show();
     return 0;
 }
+
+// 1、构造函数执行顺序：1. 先执行所包含的对象的构造函数，再执行自己的构造函数
+// 2、析构函数执行顺序：1. 先执行对象自己的析构函数，再执行所包含的对象的析构函数
