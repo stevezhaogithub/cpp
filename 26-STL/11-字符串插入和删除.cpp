17e2e545315cb66e9d41e88c40cd6d2f6d0569b6
steve xiaohu zhao <steve_zhao@163.com>
2025-10-25 09:57:33 +0800

    字符串截取

--- 26-STL/11-字符串插入和删除.cpp
+++ 26-STL/11-字符串插入和删除.cpp
@@ -16,8 +16,28 @@ void test01()
     cout << "str1 = " << str1 << endl;
 }
 
+// string 子串
+void test02()
+{
+    string str = "abcdefg";
+    // 截取子字符串
+    string sub_str = str.substr(1, 3);
+    cout << sub_str << endl;
+}
+
+void test03()
+{
+    string email = "hello@sina.com.cn";
+    int idx = email.find('@', 0);
+    // 从索引为 0 开始，截取 idx 个字符
+    string _user_name = email.substr(0, idx);
+    cout << _user_name << endl;
+}
+
 int main()
 {
-    test01();
+    // test01();
+    // test02();
+    test03();
     return 0;
 }
