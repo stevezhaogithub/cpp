1f1d0f87f11df24b8f32d38f4a6d238c72fec680
steve xiaohu zhao <steve_zhao@163.com>
2025-11-04 16:34:53 +0800

    add Administrator class

---
+++ 28-book-system/administrator.cpp
@@ -0,0 +1,35 @@
+#include "administrator.h"
+// 默认的无参数构造函数
+Administrator::Administrator()
+{
+}
+
+// 有参数构造函数
+Administrator::Administrator(string _name, string _pwd)
+{
+}
+
+// 显示菜单
+void Administrator::show_menu()
+{
+}
+
+// 添加账号
+void Administrator::add_account()
+{
+}
+
+// 查看账号
+void Administrator::show_accounts()
+{
+}
+
+// 查看机房信息
+void Administrator::show_rooms()
+{
+}
+
+// 清空预约
+void Administrator::clear_reservations()
+{
+}
---
+++ 28-book-system/administrator.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+#include "identity.h"
+using namespace std;
+
+// 管理员类
+class Administrator : public Identity
+{
+public:
+    // 默认的无参数构造函数
+    Administrator();
+
+    // 有参数构造函数
+    Administrator(string _name, string _pwd);
+
+    // 显示菜单
+    virtual void show_menu();
+
+    // 添加账号
+    void add_account();
+
+    // 查看账号
+    void show_accounts();
+
+    // 查看机房信息
+    void show_rooms();
+
+    // 清空预约
+    void clear_reservations();
+};
