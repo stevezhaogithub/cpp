44006c22e37fb1dc2c50eedadc708fada9b3e276
steve xiaohu zhao <steve_zhao@163.com>
2025-11-04 16:39:11 +0800

    add global_file.h

---
+++ 28-book-system/global_file.h
@@ -0,0 +1,15 @@
+/*
+    不同的身份可能会用到不同的文件操作
+    将所有的文件名定义到一个全局文件中
+*/
+#pragma once
+// 管理员文件
+#define ADMIN_FILE "admin.txt"
+// 学生文件
+#define STUDENT_FILE "student.txt"
+// 教师文件
+#define TEACHER_FILE "teacher.txt"
+// 机房信息文件
+#define ROOM_FILE "labs.txt"
+// 预约信息文件
+#define RESERVATION_FILE "reservation.txt"
