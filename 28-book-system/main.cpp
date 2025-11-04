6ebba25fad36d3b9c9756c48acf5647735882e4e
steve xiaohu zhao <steve_zhao@163.com>
2025-11-04 17:07:40 +0800

    add login() function.

--- 28-book-system/main.cpp
+++ 28-book-system/main.cpp
@@ -1,5 +1,72 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "identity.h"
+#include "global_file.h"
 using namespace std;
+
+// 登录功能
+/**
+ * _fname: 操作文件的名称
+ * _role: 操作身份的类型（角色)
+ */
+void login(string _fname, int _role)
+{
+    // 创建一个父类指针，将来用于指向子类对象
+    Identity *person = NULL;
+
+    // 读文件
+    ifstream ifs;
+    ifs.open(_fname, ios::in);
+    // 判断文件是否存在
+    if (!ifs.is_open())
+    {
+        // 文件打开失败
+        cout << "文件不存在！" << endl;
+        ifs.close();
+        return;
+    }
+    // 准备接收用户信息
+    int _id = 0;
+    string _name, _pwd;
+
+    // 判断身份
+    switch (_role)
+    {
+    case 1:
+        cout << "请输入学号: " << endl;
+        cin >> _id;
+        break;
+    case 2:
+        cout << "请输入职工号: " << endl;
+        cin >> _id;
+        break;
+    }
+
+    cout << "请输入用户名: ";
+    cin >> _name;
+    cout << "请输入密码: ";
+    cin >> _pwd;
+    // 验证身份
+    if (_role == 1)
+    {
+        // 学生身份验证
+    }
+    else if (_role == 2)
+    {
+        // 教师身份验证
+    }
+    else if (_role == 3)
+    {
+        // 管理员身份验证
+    }
+    cout << "验证登录失败！" << endl;
+    cin.ignore();
+    cout << "按 Enter 键继续..." << endl;
+    cin.get();
+    system("clear");
+}
+
 int main()
 {
     // 接收用户的选择
@@ -30,12 +97,15 @@ int main()
         {
         case 1:
             // 学生身份
+            login(STUDENT_FILE, 1);
             break;
         case 2:
             // 老师身份
+            login(TEACHER_FILE, 2);
             break;
         case 3:
             // 管理员身份
+            login(ADMIN_FILE, 3);
             break;
         case 0:
             // 退出程序
